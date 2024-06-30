318c8b894cefe596ffe79db6bc3480d7203b12d3
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2024-06-30 18:02:38 +0300

    Add custom platform example

---
+++ example/07.custom-platform.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+use TypeLang\Mapper\Mapper;
+use TypeLang\Mapper\Registry\Registry;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+// The set of types and grammar is defined using a "platform". You can create
+// your own platform, for example, for a specific DB, or use built-in ones.
+//
+// For example, let's create a platform that supports only simple types,
+// without generics, union types, shapes, and other things.
+
+class SimplePlatform implements \TypeLang\Mapper\PlatformInterface
+{
+    public function getName(): string
+    {
+        return 'simple';
+    }
+
+    public function getBuiltinTypes(): iterable
+    {
+        // The platform will only support objects, that is,
+        // references to existing classes.
+        yield new \TypeLang\Mapper\Type\Builder\ObjectTypeBuilder();
+    }
+
+    public function isFeatureSupported(\TypeLang\Mapper\Platform\GrammarFeature $feature): bool
+    {
+        // Disable all grammar features except the main one.
+        return false;
+    }
+}
+
+class ExampleDTO
+{
+    public function __construct(
+        public readonly int $value = 42,
+    ) {}
+}
+
+$mapper = new Mapper(
+    types: new Registry(
+        platform: new SimplePlatform(),
+    ),
+);
+
+var_dump($mapper->normalize(new ExampleDTO()));
+//
+// TypeRequiredException: The ExampleDTO::$value property contains an
+//                        unregistered type that cannot be explicitly converted
+//
+
+var_dump($mapper->normalize([new ExampleDTO()], 'array<ExampleDTO>'));
+//
+// ParseException: Template arguments not allowed in "array<ExampleDTO>" at column 6
+//
